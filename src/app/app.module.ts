7bdba7d2c72b910839eaacd8dfb9ff27275b9c05
Gustavo Freitas <gustavofreitas_silva@hotmail.com>
2023-05-24 20:02:19 -0300

    menu title feito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { MeuTitleComponent } from './components/meu-title/meu-title.component';
-import { MeuEmailComponent } from './components/meu-email/meu-email.component';
-import { MeuFotoComponent } from './components/meu-foto/meu-foto.component';
-import { MeuTitleInfo01Component } from './components/meu-title-info01/meu-title-info01.component';
-import { MeuTitleInfo02Component } from './components/meu-title-info02/meu-title-info02.component';
-import { MeuCardInfo01Component } from './components/meu-card-info01/meu-card-info01.component';
-import { MeuCardInfo02Component } from './components/meu-card-info02/meu-card-info02.component';
+import { MeuBigCardComponent } from './components/meu-big-card/meu-big-card.component';
+import { MeuSmallCardComponent } from './components/meu-small-card/meu-small-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuBarComponent,
     MeuTitleComponent,
-    MeuEmailComponent,
-    MeuFotoComponent,
-    MeuTitleInfo01Component,
-    MeuTitleInfo02Component,
-    MeuCardInfo01Component,
-    MeuCardInfo02Component
+    MeuBigCardComponent,
+    MeuSmallCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/meu-big-card/meu-big-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-big-card',
+  templateUrl: './meu-big-card.component.html',
+  styleUrls: ['./meu-big-card.component.css']
+})
+export class MeuBigCardComponent {
+
+}
