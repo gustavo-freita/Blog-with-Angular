47662001d1e5565c50a491cbec71b2f129722e23
Gustavo Freitas <gustavofreitas_silva@hotmail.com>
2023-05-26 17:57:55 -0300

    deixando a página dinamica

--- src/app/components/meu-big-card/meu-big-card.component.ts
+++ src/app/components/meu-big-card/meu-big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-meu-big-card',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class MeuBigCardComponent {
 
+  @Input()
+  photoCover:string = ""
+  @Input()
+  cardTitle:string = ""
+  @Input()
+  cardDescription:string = ""
 }
--- src/app/components/meu-small-card/meu-small-card.component.ts
+++ src/app/components/meu-small-card/meu-small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-meu-small-card',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./meu-small-card.component.css']
 })
 export class MeuSmallCardComponent {
+
+  @Input()
+  cardFoto:string = ""
+  @Input()
+  cardTitulo:string = ""
+  @Input()
+  cardDescricao:string = ""
 
 }
