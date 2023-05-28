433591a8f599e290a5b0559fc3b08e24f71e03a7
Gustavo Freitas <gustavofreitas_silva@hotmail.com>
2023-05-28 08:20:12 -0300

    deixando a segunda página dinamica com TypeScript

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
   {
-    "id":1,
-    "photo":"aaa",
-    "title":"lorem lorem",
-    "description":"lorem lorem lorem"
+    "id":"1",
+    "title":"Lorem ipsum, dolor sit amet consectetur.",
+    "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur sed consectetur explicabo, sint repellat obcaecati molestias aliquid aspernatur rem. Autem eum laboriosam sit saepe dolore tenetur. Voluptates dolor cum tenetur!",
+    "photoCover": "https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png"
   },
   {
-    "id":2,
-    "photo":"aaa",
+    "id":"2",
     "title":"lorem lorem",
-    "description":"lorem lorem lorem"
+    "description":"lorem lorem lorem",
+    "photoCover": ""
   }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake} from '../../data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -8,9 +9,10 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ContentComponent {
 
-  photoCover:string = "https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png"
-  contentTitel:string = "Lorem ipsum, dolor sit amet consectetur."
-  contentDescription:string = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur sed consectetur explicabo, sint repellat obcaecati molestias aliquid aspernatur rem. Autem eum laboriosam sit saepe dolore tenetur. Voluptates dolor cum tenetur!"
+  photoCover:string = ""
+  contentTitel:string = ""
+  contentDescription:string = ""
+  private id:string | null ="0"
 
   constructor (
     private route:ActivatedRoute
@@ -18,7 +20,17 @@ export class ContentComponent {
 
   ngOnInit(): void {
     this.route.paramMap.subscribe( value =>
-      console.log(value.get("id"))
-    );
+      this.id = value.get("id")
+    )
+
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id:string | null) {
+    const result = dataFake.filter(article => article.id.toString() == id)[0]
+
+    this.contentTitel = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
   }
 }
